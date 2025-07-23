ff453e964c06c6041799be0e9a50a85238d75f9c
未來星織 <bakadax@qq.com>
2025-07-23 17:17:30 +0800

    fix: 迁移打包配置至 pyproject.toml (#5)
    将项目元数据和打包配置从 setup.py 完全迁移到 pyproject.toml...

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
 import setuptools
 
-with open("README.md", encoding="utf-8") as fh:
-    long_description = fh.read()
+setuptools.setup()
 
-AICARUS_PROTOCOL_VERSION = "1.6.0"  # 与你文档中的版本号保持一致
-
-setuptools.setup(
-    name="aicarus_protocols",
-    version=AICARUS_PROTOCOL_VERSION,
-    author="Dax233",
-    author_email="bakadax@qq.com",
-    description="AIcarus 项目核心与适配器之间的通信协议定义库",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/AIcarusDev/AIcarusProtocols",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    # Python 版本要求
-    python_requires=">=3.12",
-)
